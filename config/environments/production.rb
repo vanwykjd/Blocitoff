2392cc2930e56157163b40699948ff762b76500b
Josh Van Wyk <joshvanwyk@ymail.com>
2016-07-29 02:05:59 +0000

    action mailer url update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { host: 'https://intense-woodland-73593.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://infinite-everglades-33867.herokuapp.com' }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
