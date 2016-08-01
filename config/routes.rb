bcacf7e5784da021885f4bd65eeff8d6a139be27
Josh Van Wyk <joshvanwyk@ymail.com>
2016-08-01 19:02:57 +0000

    user/auth_user routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:show]
   
   authenticated :user do
     root to: 'users#show', as: :authenticated_root
