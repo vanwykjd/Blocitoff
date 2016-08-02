64db03fcf94aa55bc9a1c4d3cffbafd53a4c5593
Josh Van Wyk <joshvanwyk@ymail.com>
2016-08-02 19:39:18 +0000

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   
   devise_for :users
   
-  resources :users, only: [:new, :create]
+  resources :users, only: [:show] do 
+    resources :items, only: [:create, :destroy]
+  end
 
-  
- 
+  authenticated :user do
+    root to: 'users#show', as: :authenticated_root
+  end 
   
   get 'welcome/index'
 
