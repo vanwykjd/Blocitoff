265effff4211329a49199fe3f225cd5ab881838f
Josh Van Wyk <joshvanwyk@ymail.com>
2016-08-02 21:18:30 +0000

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,20 @@ Rails.application.routes.draw do
   
   devise_for :users
   
-  resources :users, only: [:new, :create]
-   authenticated :user do
-     root to: 'users#show', as: :authenticated_root
-   end 
-   
-   get 'welcome/index'
- 
-   get 'welcome/about'
-   
-   root 'welcome#index'
+  resources :users, only: [:show] do 
+    resources :items, only: [:create, :destroy]
+  end
+
+  authenticated :user do
+    root to: 'users#show', as: :authenticated_root
+  end 
+  
+  get 'welcome/index'
+
+  get 'welcome/about'
+  
+  root 'welcome#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
