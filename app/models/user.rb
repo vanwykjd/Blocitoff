95961076f37169ab3247a61c329e933931b991b2
Josh Van Wyk <joshvanwyk@ymail.com>
2016-08-04 04:55:59 +0000

    sign-in_sign-out | spec users#show failing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,27 @@ class User < ActiveRecord::Base
   # Virtual attribute for authenticating by either username or email
   # This is in addition to a real persisted field like 'username'
   attr_accessor :login
+
   
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  validates :username, :presence => true, :uniqueness => { :case_sensitive => false  }
-
+  
+  
+  
   def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
+      conditions = warden_conditions.dup
       if login = conditions.delete(:login)
-        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-      else
-      if conditions[:email].nil?
-        where(conditions).first
-      else
-        where(email: conditions[:email]).first
-      end
+          where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+        else
+        if conditions[:email].nil?
+          where(conditions).first
+        else
+          where(email: conditions[:email]).first
+        end
       end
   end
+
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :user do
     username "vanwykjd"
-    email "joshvanwyk@ymail.com"
-    password_digest "password"
+    sequence(:email, 100) { |n| "person#{n}@example.com" }
+    password "password"
+    password_confirmation "password"
+    unconfirmed_email false
+    confirmed_at Time.now
+    after(:create) { |user| user.confirm_token = nil }
+    after(:create) { |user| user.confirm_digest = nil }
+    after(:create) { |user| EncryptPassword.call(user: user) }
   end
 end
